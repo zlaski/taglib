770b0829ba429c562ffb0be6a9e56fa099719f72
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-03-18 15:19:42 -0700

    Delete old vcxproj cruft

--- taglib-headers.c
+++
@@ -1,2 +0,0 @@
-// here so that the library is actually created
-int __akibld_taglib_headers(int x, int y) { return x * y; }
--- taglib.pre.bat
+++ taglib.pre.bat
@@ -6,3 +6,24 @@ set "ProjectDir=%~dp0"
 set "SolutionPath=%~1"
 set "IntDir=%~2"
 set "OutDir=%~3"
+
+
+pushd "%ProjectDir%"
+
+set QDIR="%OutDir%..\include\taglib\"
+mkdir %QDIR%
+
+for /F %%H in (taglib-headers.lst) do (
+    echo %%H
+    xcopy /i /r /y /q %%H %QDIR% >nul
+)
+
+echo taglib\taglib_config.h.cmake
+echo EMPTY >%QDIR%taglib_config.h
+xcopy /r /y /q taglib\taglib_config.h.cmake %QDIR%taglib_config.h >nul
+
+pushd %QDIR%
+attrib +R /S
+popd
+
+popd
